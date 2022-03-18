cbbdd2cbe764516f4b5ca1c60a5fa865ef30af5a
Tushar <tusharkumarsahu1998@gmail.com>
2022-03-18 16:13:11 +0530

    My YouTube

---
+++ component/nav.js
@@ -0,0 +1,19 @@
+function nav (){
+    return ` <div id="ytdiv">
+            <div id="lin"><i class="fas fa-align-justify"></i></div>
+            <div id="yt"><i class="fab fa-youtube"></i></div>
+            <h2>YouTube</h2>
+        </div>
+            
+        <div id="box">
+        <input id="search" type="search" placeholder="Search..">
+        <button   id="submit" type="submit"><i class="fas fa-search"></i></button>
+        <div id="microphone"><i class="fas fa-microphone"></i></div>
+        </div>
+
+        <div id="fa-bell">
+        <div id="fa-belll"><i class="far fa-bell"></i></div>
+        <div id="user-circle"><i class="far fa-user-circle"></i></div>
+        </div>`
+}
+export default nav
---
+++ component/rit.js
@@ -0,0 +1,113 @@
+function rit (){
+    return ` <div id="home" style="background-color: #e5e5e5;cursor: pointer; ">
+                <div id="homee"><i class="fas fa-home"></i></div>
+            <h3 style="margin-right: 110px;">Home</h3>
+            </div>
+
+            <div id="home">
+            <div id="homee"><i class="far fa-compass"></i></div>
+            <h3 style="margin-right: 88px;">Explore</h3>
+            </div>
+
+            <div id="home">
+            <div id="homee"><i class="fas fa-money-bill-wave"></i></div>
+            <h3 style="margin-right: 30px;">Subscriptions</h3>
+            </div>
+            <hr>
+
+            <div id="home">
+            <div id="homee"><i class="fas fa-book-open"></i></div>
+            <h3>Library</h3>
+            </div>
+
+            <div id="home">
+            <div id="homee"><i class="fas fa-history"></i></div>
+            <h3>History</h3>
+            </div>
+
+            <div id="home">
+            <div id="homee"><i class="far fa-clock"></i></div>
+            <h3>Watch Later</h3>
+            </div>
+
+            <div id="home">
+            <div id="homee"><i class="far fa-thumbs-up"></i></div>
+            <h3>Liked Video</h3>
+            </div>
+            <hr>
+            <h3 style="margin-left:px ;">SUBSCRIPTIONS</h3>
+
+            <div id="home">
+            <div id="homee"><i class="fas fa-music"></i></div>
+            <h3>Music</h3>
+            </div>
+
+            <div id="home">
+            <div id="homee"><i class="fas fa-trophy"></i></div>
+            <h3>Sports</h3>
+            </div>
+
+            <div id="home">
+            <div id="homee"><i class="fab fa-d-and-d"></i></div>
+            <h3>Gaming</h3>
+            </div>
+
+            <div id="home">
+            <div id="homee"><i class="fas fa-film"></i></div>
+            <h3>Movies</h3>
+            </div>
+
+            
+
+            <hr>
+            <h3>MORE FROM YOUTUBE</h3>
+
+            <div id="home">
+            <div id="homee"><i class="fas fa-home"></i></div>
+            <h3>Youtube Premium</h3>
+            </div>
+
+            <div id="home">
+            <div id="homee"><i class="fas fa-wifi"></i></div>
+            <h3>Live</h3>
+            </div>
+
+            <div id="home">
+            <div id="homee"><i class="fas fa-vest-patches"></i></div>
+            <h3>Fashion</h3>
+            </div>
+
+            <div id="home">
+            <div id="homee"><i class="fas fa-graduation-cap"></i></div>
+            <h3>Learning</h3>
+            </div>
+
+
+
+            <hr>
+
+            <div id="home">
+            <div id="homee"><i class="fas fa-cogs"></i></div>
+            <h3>Settings</h3>
+            </div>
+
+            <div id="home">
+            <div id="homee"><i class="far fa-flag"></i></div>
+            <h3>Report</h3>
+            </div>
+
+            <div id="home">
+            <div id="homee"><i class="fas fa-hands-helping"></i></div>
+            <h3>Help</h3>
+            </div>
+
+            <div id="home">
+            <div id="homee"><i class="far fa-comments"></i></div>
+            <h3>Send Feedback</h3>
+            </div>
+
+`
+
+          
+}
+export default rit
